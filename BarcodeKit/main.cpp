9c09a66db6ae69ee725e73c2d8479837e6b51f34
Robert <robstearn@me.com>
2012-04-09 09:06:33 +0100

    First Xcode Commit

---
+++ BarcodeKit/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  BarcodeKit
+//
+//  Created by Robert Stearn on 09.04.12.
+//  Copyright (c) 2012 Cocoadelica. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
