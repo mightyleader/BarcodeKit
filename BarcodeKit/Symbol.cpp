92368d5660d77a39c51861cc6941e7b5d2661656
Robert <robstearn@me.com>
2012-04-14 14:01:34 +0100

    Completed header, completed outline declarations in main. Added vectors for dynamic arrays requirements

--- BarcodeKit/Symbol.cpp
+++ BarcodeKit/Symbol.cpp
@@ -1,9 +1,93 @@
 //
 //  Symbol.cpp
+//	Object represents a single character, data or non-data, storing it's attributes and encoding
 //  BarcodeKit
 //
 //  Created by Robert Stearn on 13.04.12.
 //  Copyright (c) 2012 Cocoadelica. All rights reserved.
 //
 
+#include "Symbol.h"
 #include <iostream>
+
+using namespace std;
+
+Symbol::Symbol( )
+{
+	
+}
+
+Symbol::Symbol( int characterData[ ], int dataLength )
+{
+	
+}
+
+Symbol::Symbol( int characterData[ ], int dataLength, int startWith )
+{
+	
+}
+
+Symbol::Symbol( int characterData[ ], int dataLength, int startWith, int gapWidth )
+{
+	
+}
+
+Symbol::Symbol( int characterData[ ], int dataLength, int startWith, int gapWidth, int ofType )
+{
+	
+}
+
+int getLeadingElement( )
+{
+	return 1;
+}
+
+int getIntercharGap( )
+{
+	return 1;
+}
+
+int getSymbolType( )
+{
+	return 1;
+}
+
+vector<int>* getEncodedData( )
+{
+	
+}
+
+int getForcePostion( )
+{
+	return 1;
+}
+
+void setLeadingElement( int le )
+{
+	
+}
+
+void setIntercharGap( int icg )
+{
+	
+}
+
+void setSymbolType( int st )
+{
+	
+}
+
+void setEncodedData( vector<int>* ec )
+{
+	
+}
+
+void setForcedPosition( int fp )
+{
+	
+}
+
+bool Symbol::arrayIntoVector( int source[ ], int sourceLength, vector<int>* destination)
+{
+	return 1;
+}
--- BarcodeKit/Symbol.h
+++ BarcodeKit/Symbol.h
@@ -1,5 +1,6 @@
 //
 //  Symbol.h
+//	Object represents a single character, data or non-data, storing it's attributes and encoding
 //  BarcodeKit
 //
 //  Created by Robert Stearn on 13.04.12.
@@ -9,48 +10,44 @@
 #ifndef BarcodeKit_Symbol_h
 #define BarcodeKit_Symbol_h
 
+#include <vector>
+
 using namespace std;
 
 class Symbol
 {
 	public:
-		Symbol(); //default constructor
-		
+	//Build and destroy
+	Symbol( );																					//default constructor
+	Symbol( int characterData[ ], int dataLength );												//construct with passed data...
+	Symbol( int characterData[ ], int dataLength, int startWith );								//...with data and leading flag...
+	Symbol( int characterData[ ], int dataLength, int startWith, int gapWidth );				//...with data, leading flag and ic gap...
+	Symbol( int characterData[ ], int dataLength, int startWith, int gapWidth, int ofType );	//...or with everything supplied
+	~Symbol( );
+	
+	//Accessors
+	int getLeadingElement( );
+	int getIntercharGap( );
+	int getSymbolType( );
+	vector<int>* getEncodedData( );
+	int getForcePostion( );
+	
+	void setLeadingElement( int le );
+	void setIntercharGap( int icg );
+	void setSymbolType( int st );
+	void setEncodedData( vector<int>* ec );
+	void setForcedPosition( int fp );
 	
 	
 	private:
-		int leadingElement;
-		int intercharacterGap;
-		int symbolType;
-		int[] encodedSymbol;
-}
-
-//Leading Element (integer)
-//Does the symbol start with a bar or a space.
-//0 - space
-//1 - bar
-//
-//Intercharacter Gap (integer)
-//What’s the dimension of the intercharacter gap as a multiple of X?
-//0 - no gap (0X)
-//1..n - Gap is 1X...nX wide
-//
-//Symbol Type (integer?)
-//What type of symbol does the encoding represent, data or non-data?
-//0 - Data
-//1 - Check Character
-//2 - Quiet Zone
-//3 - Guard Pattern
-//
-//Data (integer[ ])
-//Array of values representing widths of each elements in terms of X.
-//1 - 1X
-//2 - 2X
-//3 - 3X
-//4 - 4X
-
-
-
-
+	//ivars
+	int leadingElement;																			//0 for space, 1 for bar
+	int intercharacterGap;																		//0..n as multiple of X width
+	int symbolType;																				//0,1..4 indicates data or specific non-data type
+	vector<int>encodedSymbol;																	//vector of element widths as multiples of X width
+	int forcePosition;																			//int value to specify ultimate position in destination array
+	
+	bool arrayIntoVector( int source[ ], int sourceLength, vector<int>* destination);
+};
 
 #endif
