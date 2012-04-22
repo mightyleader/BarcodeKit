54d28c3735dedda462457e4e296961cb6c8e81fc
Robert <robstearn@me.com>
2012-04-22 14:03:41 +0100

    More work on method implementation in the Base class
    Some issues with vector insertion to work out.

--- BarcodeKit/BaseBarcode.cpp
+++ BarcodeKit/BaseBarcode.cpp
@@ -1,7 +1,7 @@
 //
 //  BaseBarcode.cpp
 //  BarcodeKit
-//
+//	Pure Virtual Abstract Base Class for Barcode class heirarchy
 //  Created by Robert Stearn on 22.04.12.
 //  Copyright (c) 2012 Cocoadelica. All rights reserved.
 //
@@ -47,27 +47,42 @@ int BaseBarcode::getDataLength( )
 
 void BaseBarcode::addEncodedPatternData( Symbol* pattern )
 {
-	
+	this->encodingpatternData.push_back( pattern );
 }
 
 void BaseBarcode::addEncodedPatternNonData( Symbol* pattern )
 {
-	
+	this->encodingPatternNondata.push_back( pattern );
 }
 
-void addEncodedPatternData( Symbol* pattern, int position )
+void BaseBarcode::addEncodedPatternData( Symbol* pattern, int position )
 {
 	
+	switch ( position ) {
+		case 0:
+			this->encodingpatternData.insert( encodingpatternData.begin( ), pattern );
+			break;
+		//case limit:
+			
+		default:
+			break;
+	}
+	this->encodingpatternData.insert(encodingpatternData.begin(), pattern);
 }
 
-void addEncodedPatternNonData( Symbol* pattern, int position )
+void BaseBarcode::addEncodedPatternNonData( Symbol* pattern, int position )
 {
 	
 }
 
 void BaseBarcode::setQuietzoneWidths( int left, int right, int upper, int lower )
 {
-	
+	this->quietzoneWidths.clear( );
+	quietzoneWidths.push_back( left );
+	quietzoneWidths.push_back( right );
+	quietzoneWidths.push_back( upper );
+	quietzoneWidths.push_back( lower );
+							  
 }
 
 void BaseBarcode::addEncodedSymbol( Symbol* symbol )
@@ -77,12 +92,12 @@ void BaseBarcode::addEncodedSymbol( Symbol* symbol )
 
 void BaseBarcode::setCheckcharModulus( int modulus )
 {
-	
+	this->checkcharModulus = modulus;
 }
 
 void BaseBarcode::setDataLength( int length )
 {
-	this->dataLength = this->encodedSymbols.size();
+	this->dataLength = length;
 }
 
 #pragma mark --Private internal methods--
@@ -93,6 +108,7 @@ bool BaseBarcode::verifyData ( const string *data )
 	{
 		return true;
 	}
+	cerr << "Data does not pass verification tests" << endl;
 	return false;
 }
 
@@ -102,10 +118,12 @@ bool BaseBarcode::verifyLength ( const int length )
 	{
 		return true;
 	}
+	cerr << "Data string length does not pass verification" << endl;
 	return false;
 }
 
 bool BaseBarcode::verifyContent ( const string *content )
 {
+	cerr << "Data cannot be encoded by into a barcode-compatible format" << endl;
 	return false;
 }
