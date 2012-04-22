75a269bcdff7160e8c04afd1fe3fc09a3d418707
Robert <robstearn@me.com>
2012-04-22 22:10:40 +0100

    Base128 Constructor in progress
    Using RapidXML to parse and create ivar structure

--- BarcodeKit/Base128.cpp
+++ BarcodeKit/Base128.cpp
@@ -7,12 +7,16 @@
 //
 
 #include <iostream>
+#include <fstream>
 #include <vector>
 #include <deque>
 #include <string>
 #include "Base128.h"
 #include "Symbol.h"
 #include "rapidxml.hpp"
+
+using namespace std;
+using namespace rapidxml;
 
 #pragma mark --Con/Destructors--
 
@@ -23,6 +27,47 @@ Base128::Base128( string *data )
 	//iterate through DOM
 	
 	//add entries into data structure
+	
+	ifstream xmlfile ( "xml_test.xml", ios::in );		//setup file for input...
+	vector<string> xmlcontent;							//...and the vars we'll use 
+	string xmlentry;
+	string xmltoparse;
+	
+	if ( xmlfile.is_open( ) )							//open the file
+	{
+		while ( getline( xmlfile, xmlentry ) )			//get the data
+		{
+			xmlcontent.push_back( xmlentry + "\n" );	//add data to vector
+		}
+		xmlfile.close( );								//close the file
+		
+		for ( int i = 0; i < xmlcontent.size( ); i++ )	//iterate through vector 
+		{
+			xmltoparse += xmlcontent[ i ];				//add it to the string
+		}
+	}
+	char * cxml = new char [ xmltoparse.size( ) + 1 ];	
+	strcpy ( cxml, xmltoparse.c_str( ) );				//convert string into cstring req'd by API
+	
+	xml_document< > parsed_xml;							//setup DOM object...
+	parsed_xml.parse< 0 >( cxml );						//...pass cstring to parser to parse into DOM object
+	
+	xml_node< > *node = NULL;
+	node = parsed_xml.first_node( )->first_node()->next_sibling()->first_node();
+	
+	while ( node->next_sibling( ) != 0 ) 
+	{
+		cout << node->name( );
+		xml_node< > *datanode = node->first_node();
+		while (datanode != 0) 
+		{
+			cout << " : " << datanode->value();
+			datanode = datanode->next_sibling();
+		}
+		cout << endl;
+		node = node->next_sibling( );
+	}
+	
 }
 
 
