a2f782323cf0883259b84d931b7efd1fc5fd7f4c
Robert <robstearn@me.com>
2012-04-27 22:24:51 +0100

    Main.cpp stage 2, handling different character sets
    Started to add code to determine the difference between A, B & C sets

--- BarcodeKit/main.cpp
+++ BarcodeKit/main.cpp
@@ -18,7 +18,7 @@
 using namespace std;
 using namespace rapidxml;
 
-Symbol* createSymbol( int st, int ic, int le, int fp, vector<int> *aVector );
+Symbol* createSymbol( int st, int ic, int le, int fp, vector< int > *aVector );
 void testSymbol( Symbol *testSymbol );
 char* getXMLToParse( string *fileTitle );
 void testDOM ( xml_node< > *node );
@@ -31,7 +31,7 @@ int main( int argc, const char * argv[ ] )
 	//****************************
 	
 	//Unit Test Data for Symbol
-	vector<int> *testVector = new vector<int>( );
+	vector< int > *testVector = new vector< int >( );
 	testVector->push_back( 1 );
 	testVector->push_back( 2 );
 	testVector->push_back( 3 );
@@ -44,10 +44,7 @@ int main( int argc, const char * argv[ ] )
 	//testSymbol( versionOne );
 	
 	delete testVector;
-	
 	//End Unit Test - Symbol
-	
-	
 	
 	//****************************
 	//** RapidXML Test Code ******
@@ -71,7 +68,6 @@ int main( int argc, const char * argv[ ] )
 	node = parsed_xml.first_node( )->first_node( )->next_sibling( "data_encoding" )->first_node( )->first_node();
 	//testDOM( node );
 	
-	
 	//*****************************
 	//** Putting it all together **
 	//*****************************
@@ -82,7 +78,7 @@ int main( int argc, const char * argv[ ] )
 	int stringLength = incomingString->length( );
 	int asciiList[ stringLength ];
 	//loop thru the data and fill the array with ascii values.
-	for (int ii = 0; ii < stringLength; ii++) 
+	for ( int ii = 0; ii < stringLength; ii++ ) 
 	{
 		char eachChar = incomingString->at( ii );
 		asciiList[ ii ] = ( int )eachChar;
@@ -96,28 +92,60 @@ int main( int argc, const char * argv[ ] )
 		string secondBit;
 		stringstream out;
 		out << kk;
-		secondBit = out.str();
+		secondBit = out.str( );
 		firstBit.append( secondBit );
-		
 		
 		node = parsed_xml.first_node( )->first_node( )->next_sibling( "data_encoding" )->first_node( firstBit.c_str( ) );
 		
+		// IF C THEN DO C THINGS
+		
+		//*******SET C SPECIFIC**********
+		//which set are we taking the char from? THIS IS FOR BASE128 DERIVED CLASSES ONLY
+		//Use Set C? 
+		char eachChar = incomingString->at( jj );
+		char nextChar = incomingString->at( jj );
+		int charSetToRef = 0;
+		if ( isdigit( eachChar ) && isdigit( nextChar )  ) //Test for consecutive digits
+		{
+			charSetToRef = 3;
+			//TODO increment counter by 1 and store the concat. numbers into a string and use it to determine pattern 
+		}
+		
+		//ELSE IF A OR B DO A/B THINGS
+		
+		//*******A & B FROM HERE*********
+		
 		//get the values for the tag back as a vector
 		vector<string> returnedData = returnDOMValues( node );
-		cout << returnedData.at( 0 ) << " - " << returnedData.at( 1 ) << " - " << returnedData.at( 2 ) << endl;
+		cout << returnedData.at( 0 ) << " - " << returnedData.at( 1 ) << " - " << returnedData.at( 2 ) << " - " << returnedData.at( 3 ) << endl; //DEBUG
+		
+		
+		//Use Set B?
+		if ( returnedData.at( 1 ) == "nil" ) 
+		{
+			charSetToRef = 2;
+		}
+		//OK, Set A it is.
+		else 
+		{
+			charSetToRef = 1;
+		}
 		
 		vector< int > *pattern = new vector< int >;
-		for ( int ll = returnedData.at( 1 ).length( ) - 1 ; ll >= 0 ; ll--) 
+		for ( int ll = 0; ll < returnedData.at( charSetToRef ).length( ) ; ll++)
 		{
-			string tempString = returnedData.at( 1 );
-			int temp = atoi( &tempString.at( ll ) );
-			pattern->push_back( temp ); //hmmmm...
-			cout << tempString.at( ll ) << endl;
-			cout << temp << endl;
+			char eachCharFromResult = returnedData.at( charSetToRef )[ ll ];
+			int temp = atoi( &eachCharFromResult );
+			pattern->push_back( temp );
+			cout << ll << ". " << temp << endl;
 		}
+		
+		//******END A & B SPECIFIC CODE******
+		
+		//UNITE A B C PATHS TO CREATE SYMBOL FROM OUTPUT OF CONDITIONAL
 
 		//use them to create a Symbol
-		Symbol *aSymbol = createSymbol( 0, 0, 0, 0, pattern );
+		Symbol *aSymbol = createSymbol( 1, 0, 1, 0, pattern ); //Default values for Base128 and BaseEANUPC except data
 		
 		//test the symbol object
 		testSymbol( aSymbol );
@@ -156,7 +184,7 @@ void testSymbol( Symbol *testSymbol ) //Output the values of a Symbol object to 
 		cout << testSymbol->getEncodedData( )->at( ii );
 	}
 	cout << endl;
-	cout << testSymbol->getEncodedData()->size() << endl;
+	cout << endl;
 }
 
 char* getXMLToParse( string *fileTitle ) //Safely get the XML file into a c_string
@@ -213,18 +241,18 @@ vector<string> returnDOMValues( xml_node< > *node ) //Return contents of a singl
 	vector<string> *returnValues = new vector<string>;
 	cout << node->name( ) << ":";
 	xml_node< > *datanode = node->first_node( );
-	while (datanode != 0) 
+	while ( datanode != 0 ) 
 	{
 		cout << " - " << datanode->name( );
 		xml_node< > *childnode = datanode->first_node( );
-		while (childnode != 0) 
+		while ( childnode != 0 ) 
 		{
 			cout << childnode->name( ) << " is " << childnode->value( );
-			string aValue = childnode->value();
+			string aValue = childnode->value( );
 			returnValues->push_back( aValue );
 			childnode = childnode->next_sibling( );
 		}
-		datanode = datanode->next_sibling();
+		datanode = datanode->next_sibling( );
 	}
 	cout << endl;
 	node = node->next_sibling( );
