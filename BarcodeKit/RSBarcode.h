6c043aae0d4cc42e1ab8b682451247d5f149e6a9
Robert <robstearn@me.com>
2012-04-28 23:44:07 +0100

    First Obj-C classes, added to place hold
    This will contain the Mac version of the barcode creators for cocoa and
    the callers for the drawing methods/objects to follow. Giggidy.

---
+++ BarcodeKit/RSBarcode.h
@@ -0,0 +1,13 @@
+//
+//  RSBarcode.h
+//  BarcodeKit
+//
+//  Created by Robert Stearn on 28.04.12.
+//  Copyright (c) 2012 Cocoadelica. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface RSBarcode : NSObject
+
+@end
---
+++ BarcodeKit/RSBarcode.m
@@ -0,0 +1,13 @@
+//
+//  RSBarcode.m
+//  BarcodeKit
+//	
+//  Created by Robert Stearn on 28.04.12.
+//  Copyright (c) 2012 Cocoadelica. All rights reserved.
+//
+
+#import "RSBarcode.h"
+
+@implementation RSBarcode
+
+@end
