98e34dc94367b9f57e178d399f98f6c1b2cddf80
Robert <robstearn@me.com>
2012-05-04 14:59:39 +0100

    New .cpp files for the 3 basic barcode subtypes

---
+++ BarcodeKit/Code39.cpp
@@ -0,0 +1,9 @@
+//
+//  Code39.cpp
+//  BarcodeKit
+//
+//  Created by Robert Stearn on 04.05.12.
+//  Copyright (c) 2012 Cocoadelica. All rights reserved.
+//
+
+#include <iostream>
---
+++ BarcodeKit/Interleaved2of5.cpp
@@ -0,0 +1,9 @@
+//
+//  Interleaved2of5.cpp
+//  BarcodeKit
+//
+//  Created by Robert Stearn on 04.05.12.
+//  Copyright (c) 2012 Cocoadelica. All rights reserved.
+//
+
+#include <iostream>
